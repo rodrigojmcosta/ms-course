b3631b4accff7b1c929f2050649af60373966696
rodrigo.costa <rodrigo.costa@squadra.com.br>
2022-07-08 17:59:43 -0300

    UserFeignClient on project hr-oauth

---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/entities/Role.java
@@ -0,0 +1,49 @@
+package com.devsuperior.hroauth.entities;
+
+import java.io.Serializable;
+
+public class Role implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private long id;
+    private String roleName;
+
+    public Role() {
+    }
+
+    public Role(long id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        return roleName != null ? roleName.equals(role.roleName) : role.roleName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return roleName != null ? roleName.hashCode() : 0;
+    }
+}
